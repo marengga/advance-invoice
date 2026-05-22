63ddc570512401084c9156084164b931672585ad
Andri K. Marengga <marengga@gmail.com>
2026-05-22 13:50:39 +0700

    fix: Enhance get_gl_entries method to handle advance settlement

--- advance_invoice/overrides/sales_invoice.py
+++ advance_invoice/overrides/sales_invoice.py
@@ -5,20 +5,31 @@ from erpnext.accounts.doctype.sales_invoice.sales_invoice import SalesInvoice
 class CustomSalesInvoice(SalesInvoice):
 	def get_gl_entries(self, warehouse_account=None):
 		gl_entries = super().get_gl_entries(warehouse_account)
-		if not self.custom_is_advance:
-			return gl_entries
 
 		company = frappe.get_doc("Company", self.company)
 		advance_account = company.default_advance_received_account
-		if not advance_account:
-			frappe.throw(f"Default Advance Received Account has not been set for {company.name}")
 
-		self.replace_income_account(gl_entries, advance_account)
+		if self.is_advance_payment_invoice:
+			if not advance_account:
+				frappe.throw(f"Default Advance Received Account has not been set for {company.name}")
+
+			self.replace_income_account(gl_entries, advance_account)
+
+		if advance_account:
+			self.replace_advance_settlement(gl_entries, advance_account)
+
 		return gl_entries
 
 	def replace_income_account(self, gl_entries, advance_account):
 		item_accounts = {d.income_account for d in self.items if d.income_account}
 
 		for gle in gl_entries:
-			if gle.account in item_accounts:
+			if gle.account in item_accounts and gle.credit > 0:
+				gle.account = advance_account
+
+	def replace_advance_settlement(self, gl_entries, advance_account):
+		settlement_accounts = {d.income_account for d in self.items if d.item_code == "ADV" and d.amount < 0}
+
+		for gle in gl_entries:
+			if gle.account in settlement_accounts and gle.debit > 0:
 				gle.account = advance_account
