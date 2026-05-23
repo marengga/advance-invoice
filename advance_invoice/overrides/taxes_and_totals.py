693460d8c611b82c18f92e6a22ce2c5c746eb93d
Andri K. Marengga <marengga@gmail.com>
2026-05-23 11:58:33 +0700

    fix: Refactor discount application logic to handle removed discounts and update net amounts

--- advance_invoice/overrides/taxes_and_totals.py
+++ advance_invoice/overrides/taxes_and_totals.py
@@ -4,52 +4,43 @@ from frappe.utils import flt
 
 class CustomTaxesAndTotals(calculate_taxes_and_totals):
 	def apply_discount_amount(self):
-		if not self.doc.discount_amount:
-			return super().apply_discount_amount()
+		super().apply_discount_amount()
 
-		positive_items = [item for item in self.doc.items if flt(item.amount) > 0]
+		if not self.doc.discount_amount:
+			return
 
 		negative_items = [item for item in self.doc.items if flt(item.amount) <= 0]
 
 		if not negative_items:
-			return super().apply_discount_amount()
+			return
+
+		positive_items = [item for item in self.doc.items if flt(item.amount) > 0]
+
+		removed_discount = 0
+
+		for item in negative_items:
+			removed_discount += flt(item.distributed_discount_amount)
+
+			item.distributed_discount_amount = 0
+
+		if not removed_discount:
+			return
 
 		positive_total = sum(flt(item.amount) for item in positive_items)
 
-		if not positive_total:
-			return super().apply_discount_amount()
+		remaining = removed_discount
 
-		remaining_discount = flt(self.doc.discount_amount)
+		precision = self.doc.precision("distributed_discount_amount")
 
 		for idx, item in enumerate(positive_items):
 			if idx == len(positive_items) - 1:
-				distributed = remaining_discount
+				additional = remaining
 			else:
-				distributed = round(
-					flt(self.doc.discount_amount) * flt(item.amount) / positive_total,
-					self.doc.precision("discount_amount"),
+				additional = round(
+					removed_discount * flt(item.amount) / positive_total,
+					precision,
 				)
 
-				remaining_discount -= distributed
+				remaining -= additional
 
-			item.distributed_discount_amount = distributed
-
-		for item in negative_items:
-			item.distributed_discount_amount = 0
-
-		self.update_item_net_amounts()
-
-		self._calculate()
-
-	def update_item_net_amounts(self):
-		for item in self.doc.items:
-			discount = flt(item.distributed_discount_amount)
-
-		item.net_amount = flt(item.amount) - discount
-
-		item.base_net_amount = flt(item.base_amount) - discount
-
-		if flt(item.qty):
-			item.net_rate = item.net_amount / flt(item.qty)
-
-			item.base_net_rate = item.base_net_amount / flt(item.qty)
+			item.distributed_discount_amount += additional
