b58d6a5595525f08b8d5ecd7009cab2449a3904d
Andri K. Marengga <marengga@gmail.com>
2026-05-25 10:55:12 +0700

    fix: Refactor discount application logic to improve handling of negative items and distribute discounts accurately

--- advance_invoice/overrides/taxes_and_totals.py
+++ advance_invoice/overrides/taxes_and_totals.py
@@ -1,46 +1,79 @@
+import frappe
 from erpnext.controllers.taxes_and_totals import calculate_taxes_and_totals
 from frappe.utils import flt
 
 
 class CustomTaxesAndTotals(calculate_taxes_and_totals):
 	def apply_discount_amount(self):
-		super().apply_discount_amount()
-
 		if not self.doc.discount_amount:
+			self.doc.base_discount_amount = 0
 			return
 
-		negative_items = [item for item in self.doc.items if flt(item.amount) <= 0]
+		if not self.doc.apply_discount_on:
+			frappe.throw("Please select Apply Discount On")
 
-		if not negative_items:
+		self.doc.base_discount_amount = flt(
+			self.doc.discount_amount * self.doc.conversion_rate,
+			self.doc.precision("base_discount_amount"),
+		)
+
+		if self.doc.apply_discount_on == "Grand Total" and self.doc.get("is_cash_or_non_trade_discount"):
+			self.discount_amount_applied = True
 			return
 
-		positive_items = [item for item in self.doc.items if flt(item.amount) > 0]
-
-		removed_discount = 0
-
+		negative_items = [item for item in self._items if flt(item.amount) <= 0]
 		for item in negative_items:
-			removed_discount += flt(item.distributed_discount_amount)
-
 			item.distributed_discount_amount = 0
 
-		if not removed_discount:
+		positive_items = [item for item in self._items if flt(item.amount) > 0]
+
+		if not positive_items:
+			self.discount_amount_applied = True
+			self._calculate()
 			return
 
-		positive_total = sum(flt(item.amount) for item in positive_items)
+		positive_total = sum(flt(item.net_amount) for item in positive_items)
+		if not positive_total:
+			self.discount_amount_applied = True
+			self._calculate()
+			return
 
-		remaining = removed_discount
+		net_total = 0
+		expected_net_total = 0
+		remaining_discount = flt(self.doc.discount_amount)
 
 		precision = self.doc.precision("distributed_discount_amount")
 
 		for idx, item in enumerate(positive_items):
 			if idx == len(positive_items) - 1:
-				additional = remaining
+				distributed_amount = remaining_discount
 			else:
-				additional = round(
-					removed_discount * flt(item.amount) / positive_total,
+				distributed_amount = flt(
+					self.doc.discount_amount * flt(item.net_amount) / positive_total,
 					precision,
 				)
 
-				remaining -= additional
+				remaining_discount -= distributed_amount
 
-			item.distributed_discount_amount += additional
+			adjusted_net_amount = item.net_amount - distributed_amount
+			expected_net_total += adjusted_net_amount
+			item.net_amount = flt(adjusted_net_amount, item.precision("net_amount"))
+			item.distributed_discount_amount = flt(
+				distributed_amount, item.precision("distributed_discount_amount")
+			)
+			net_total += item.net_amount
+
+			rounding_difference = flt(expected_net_total - net_total, self.doc.precision("net_total"))
+			if rounding_difference:
+				item.net_amount = flt(item.net_amount + rounding_difference, item.precision("net_amount"))
+				item.distributed_discount_amount = flt(
+					distributed_amount + rounding_difference,
+					item.precision("distributed_discount_amount"),
+				)
+				net_total += rounding_difference
+
+			item.net_rate = flt(item.net_amount / item.qty, item.precision("net_rate")) if item.qty else 0
+			self._set_in_company_currency(item, ["net_rate", "net_amount"])
+
+		self.discount_amount_applied = True
+		self._calculate()
